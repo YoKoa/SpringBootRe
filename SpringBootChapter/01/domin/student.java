eed938040f506960d9ee68f634df589a1e2d9756
YoKoa <2212131228@qq.com>
2017-07-29 20:45:28 +0800

    Create student.java

---
+++ SpringBootChapter/01/domin/student.java
@@ -0,0 +1,25 @@
+package com.example.demo.domin;
+
+/**
+ * Created by ZhangZhen on 2017/7/28.
+ */
+public class student {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
